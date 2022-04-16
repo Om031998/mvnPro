566c69444edfdb2c1234ecc3aecdfa1b57a0719e
akshay <kadakshay16@gmail.com>
2022-04-16 12:43:19 +0530

    My 1st push

---
+++ MyFirstMavenProjects/src/main/java/MyFirstMavenProjects/MyFirstMavenProjects/App.java
@@ -0,0 +1,13 @@
+package MyFirstMavenProjects.MyFirstMavenProjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MyFirstMavenProjects/src/test/java/MyFirstMavenProjects/MyFirstMavenProjects/AppTest.java
@@ -0,0 +1,20 @@
+package MyFirstMavenProjects.MyFirstMavenProjects;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
